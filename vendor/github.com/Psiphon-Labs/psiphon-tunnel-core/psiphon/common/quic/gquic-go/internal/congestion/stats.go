1cd2d0d945211501d9c7f4b66e47a73b7781f58b
Simone Basso <bassosimone@gmail.com>
2020-08-26 14:24:44 +0200

    delete(vendor): we can possibly avoid vendoring
    It seems I could have found a better way to guarantee that this
    repository is actually in sync. This is quite good :-).
    
    The TL;DR is `go get -v github.com/ooni/probe-engine@COMMIT`.
    
    I am going to assume this would work and zap vendor. If I am wrong,
    then, I can always re-add vendoring right away.

--- vendor/github.com/Psiphon-Labs/psiphon-tunnel-core/psiphon/common/quic/gquic-go/internal/congestion/stats.go
+++
@@ -1,8 +0,0 @@
-package congestion
-
-import "github.com/Psiphon-Labs/psiphon-tunnel-core/psiphon/common/quic/gquic-go/internal/protocol"
-
-type connectionStats struct {
-	slowstartPacketsLost protocol.PacketNumber
-	slowstartBytesLost   protocol.ByteCount
-}
--- vendor/github.com/ooni/probe-engine/internal/httpheader/accept.go
+++
@@ -1,8 +0,0 @@
-package httpheader
-
-// RandomAccept returns a random Accept header.
-func RandomAccept() string {
-	// This is the same that is returned by MK. We have #147 to
-	// ensure that headers are randomized, if needed.
-	return "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
-}
